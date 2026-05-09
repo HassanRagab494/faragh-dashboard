6caa3562d484c1cced0500c220a1c54d03459907
HassanRagab494 <hassanragab494@gmail.com>
2026-05-10 02:36:54 +0300

    fix api url

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:3000/api';
+export const API_URL = 'https://faragh-backend.vercel.app/api';
 
 export const getToken = () => localStorage.getItem('token');
 
